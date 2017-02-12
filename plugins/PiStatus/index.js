dd24aa0d6b5650a94aec12e3045bbdf793b59cb6
Fadee Kannah <fadeekannah@gmail.com>
2017-02-12 10:02:09 -0800

    Added linting to be part of the build process

--- plugins/PiStatus/index.js
+++ plugins/PiStatus/index.js
@@ -4,8 +4,8 @@ function systemUptime(req, res, next)
 {
   var uptime = os.uptime();
   var data = {
-    hours: parseInt(uptime/3600),
-    minutes: parseInt((uptime/60) % 60),
+    hours: parseInt(uptime/3600, 10),
+    minutes: parseInt((uptime/60) % 60, 10),
     seconds: uptime % 60
   };
   res.status(200).json(data);
