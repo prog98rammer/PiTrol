f1b43495ada478cbe78c5cde105cb902aacb7fc4
Fadee Kannah <fadeekannah@gmail.com>
2017-02-12 09:19:54 -0800

    Add Support for webpackProxy and switched port back to 9000

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
