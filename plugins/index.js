84cf2dc7526becf7756e5cbf83620c7fcbc29f85
Alhasan A. Al-Nasiry <alhasan.nasiry@gmail.com>
2017-02-14 23:59:35 +0300

    Duplicated to test UI

--- plugins/index.js
+++ plugins/index.js
@@ -2,6 +2,14 @@ module.exports = [
   {
     name: 'PiStatus',
     ref: require('./PiStatus')
+  },
+  {
+    name: 'PiStatus2',
+    ref: require('./PiStatus')
+  },
+  {
+    name: 'PiStatus3',
+    ref: require('./PiStatus')
   }
   // More plugins can be added here
 ]
